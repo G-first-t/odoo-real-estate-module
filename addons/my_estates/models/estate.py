f0f6edce6625d4da74cb284e1b46078640975b99
Gregory kimbira <gregorykimbira7@gmail.com>
2024-08-14 10:23:50 +0300

    adding fields

--- addons/my_estates/models/estate.py
+++ addons/my_estates/models/estate.py
@@ -5,7 +5,24 @@ class MyEstate(models.Model):
     _description="this is for real estate management"
 
 
-    
+    name=fields.Char(string="Name", required=True)
+    description=fields.Text(string="Description")
+    postcode=fields.Char(string="Postcode")
+    date_availability=fields.Date(string="Available Date",required=True)
+    expected_price=fields.Float(string="Expected Price")
+    selling_price=fields.Float(string="Selling Price")
+    bedrooms=fields.Integer(string="Bedrooms")
+    living_area=fields.Integer(string="Living Area")
+    facades=fields.Integer(string="Facades")
+    garage=fields.Boolean(string="Garage")
+    garden=fields.Boolean(string="Garden")
+    garden_area=fields.Integer(string="Garden Area")
+    garden_orientation=fields.Selection(string="Garden Orientation",
+                                        selection=[("north","north"),
+                                                   ("south","south"),
+                                                   ("east","east"),
+                                                   ("west","west")])
+
 
 
 
