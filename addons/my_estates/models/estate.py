dc46222b8049fafa488e2a23932cce6917147b9e
Gregory kimbira <gregorykimbira7@gmail.com>
2024-08-21 08:53:12 +0300

    modification

--- addons/my_estates/models/estate.py
+++ addons/my_estates/models/estate.py
@@ -23,7 +23,7 @@ class MyEstate(models.Model):
                                                    ("east","east"),
                                                    ("west","west")])
     property_type_id=fields.Many2one('property.type',string="Property Type:")
-    buyer=fields.Many2one('res.partner',string="Buyer",tracking=True, default=lambda self: self.env.user)
+    buyer=fields.Many2one('res.partner',string="Buyer", default=lambda self: self.env.user)
     seller=fields.Many2one('res.users',string="Salesman")
     tag_ids=fields.Many2many('property.tag')
     offer_ids=fields.One2many('property.offer','property_id')
